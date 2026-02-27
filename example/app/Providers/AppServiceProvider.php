92d2ee9ed5936d3fd1cfb7613a7ed8c095c6d773
MaliaRomero <romerm15@my.erau.edu>
2026-02-26 23:42:00 -0700

    Laravel Video 26

--- example/app/Providers/AppServiceProvider.php
+++ example/app/Providers/AppServiceProvider.php
@@ -26,9 +26,9 @@ class AppServiceProvider extends ServiceProvider
         //Model::preventLazyLoading();
 
         //Paginator::useBootstrapFive();
-
+/*
         Gate::define('edit-job', function (User $user, Job $job) {
             return $job->employer->user->is($user);
-        });
+        });*/
     }
 }
---
+++ example/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "malia": "rgb(54,117,136)"
+      }
+    },
+  },
+  plugins: [],
+}
+
